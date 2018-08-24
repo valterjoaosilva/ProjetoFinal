2889daf15d76ef89c69fc9a85699daa56d11692d
V008 <V008@BNU-ESTAG008>
2018-08-24 16:08:35 -0300

    projeto final paginas login e pagina de cadastro inicio

---
+++ Agenda_e_Consultas/AgendamentoConsultas/Controllers/homeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AgendamentoConsultas.Controllers
+{
+    public class homeController : Controller
+    {
+        // GET: home
+        public ActionResult Index()
+        {
+            //Regra de negocio
+            return View();
+        }
+
+    }
+}
